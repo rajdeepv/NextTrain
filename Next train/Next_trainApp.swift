6b5a141a709612afda6ec87a790c9234afad91c1
Rajdeep Varma <Rajdeep.Varma@globalrelay.net>
2025-03-04 22:17:21 +0000

    Initial Commit

---
+++ Next train/Next_trainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Next_trainApp.swift
+//  Next train
+//
+//  Created by Rajdeep Varma on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Next_trainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Next trainTests/Next_trainTests.swift
@@ -0,0 +1,17 @@
+//
+//  Next_trainTests.swift
+//  Next trainTests
+//
+//  Created by Rajdeep Varma on 04/03/2025.
+//
+
+import Testing
+@testable import Next_train
+
+struct Next_trainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
